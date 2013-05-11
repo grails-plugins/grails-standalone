7265291283526de6342ee7d7caa2330520ceab74
Burt Beckwith <burt@burtbeckwith.com>
2013-05-10 20:03:55 -0400

    1.2.1 release

--- StandaloneGrailsPlugin.groovy
+++ StandaloneGrailsPlugin.groovy
@@ -14,7 +14,7 @@
  */
 class StandaloneGrailsPlugin {
 
-	String version = '1.2'
+	String version = '1.2.1'
 	String grailsVersion = '1.3 > *'
 	def scopes = [excludes: 'war']
 	List pluginExcludes = [
